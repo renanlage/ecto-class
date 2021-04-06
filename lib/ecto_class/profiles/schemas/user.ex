2cff97ee24f47e851d5f276e1a6792060a81e14e
Luiz Carlos <lcpojr@gmail.com>
2021-04-06 18:57:58 -0300

    chore: partial class

--- lib/ecto_class/profiles/schemas/user.ex
+++ lib/ecto_class/profiles/schemas/user.ex
@@ -2,4 +2,46 @@ defmodule EctoClass.Profiles.Schemas.User do
   @moduledoc """
   Database schema representation of users table.
   """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias EctoClass.Credentials.Schemas.Password
+
+  @required_fields [:username, :document, :legal_name]
+  @optional_fields [:email_verified, :social_name, :document_type]
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "users" do
+    field :username, :string
+    field :email_verified, :boolean, default: false
+    field :social_name, :string
+    field :legal_name, :string
+    field :document, :string
+    field :document_type, :string, default: "cpf"
+
+    # Jsonb
+
+    # Relations
+    has_one :password_credential, Password, on_replace: :update, on_delete: :delete_all
+
+    timestamps()
+  end
+
+  def changeset(params) when is_map(params),
+    do: do_changeset(%__MODULE__{}, params)
+
+  def changeset(%__MODULE__{} = model, params) when is_map(params),
+    do: do_changeset(model, params)
+
+  defp do_changeset(%__MODULE__{} = model, params) do
+    model
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> cast_assoc(:password_credential, required: false, with: &Password.changeset/2)
+    |> validate_required(@required_fields)
+    |> validate_length(:legal_name, min: 2)
+    |> validate_inclusion(:document_type, ["cpf", "rg"])
+    |> unique_constraint(:username)
+  end
 end
--- lib/ecto_class/profiles/users.ex
+++ lib/ecto_class/profiles/users.ex
@@ -2,4 +2,78 @@ defmodule EctoClass.Profiles.Users do
   @moduledoc """
   Domain module for handling user database transactions.
   """
+
+  alias EctoClass.Credentials.Schemas.Password
+  alias EctoClass.Profiles.Schemas.User
+  alias EctoClass.Repo
+
+  import Ecto.Query
+
+  def create(params) when is_map(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+  end
+
+  def update(%User{} = user, params) do
+    user
+    |> User.changeset(params)
+    |> Repo.update()
+  end
+
+  def delete(%User{} = user), do: Repo.delete(user)
+
+  def all(filters \\ []) when is_list(filters) do
+    User
+    |> where(^filters)
+    |> Repo.all()
+  end
+
+  def one(filters) when is_list(filters) do
+    User
+    |> where(^filters)
+    |> Repo.one()
+  end
+
+  def exists?(filters) when is_list(filters) do
+    User
+    |> where(^filters)
+    |> Repo.exists?()
+  end
+
+  def legal_name_strats_with(legal_name) when is_binary(legal_name) do
+    User
+    |> where([u], like(u.legal_name, ^"#{legal_name}%"))
+    |> Repo.all()
+  end
+
+  @doc "Returns how many users are found with the given filters"
+  @spec count(filters :: keyword()) :: integer()
+  def count(filters \\ []) when is_list(filters) do
+    User
+    |> where(^filters)
+    |> Repo.aggregate(:count)
+  end
+
+  @doc "Returns the users ordered by the given fields"
+  @spec sort(filters :: keyword(), sort_by :: keyword()) :: list(User.t())
+  def sort(filters \\ [], sort_by \\ [desc: :inserted_at])
+      when is_list(filters) and is_list(sort_by) do
+    User
+    |> where(^filters)
+    |> order_by(^sort_by)
+    |> Repo.all()
+  end
+
+  def join(filters \\ []) do
+    User
+    |> join(:inner, [u], p in Password, on: u.id == p.user_id)
+    |> where(^filters)
+    |> select([u, p], %{u | password_credential: p})
+    |> Repo.all()
+  end
+
+  def preload([%User{} | _] = users) do
+    Repo.preload(users, [:password_credential])
+  end
 end
